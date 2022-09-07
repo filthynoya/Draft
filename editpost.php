ce575dd4c44987c14bc1a282d749beb295991546
Ayon Raihan <ayonraihan1156@gmail.com>
2022-09-07 22:35:51 +0600

    designed forget pass

--- editpost.php
+++ editpost.php
@@ -81,7 +81,7 @@
     integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
    
-    <title>Edit|Post</title>
+    <title>Edit | Post</title>
 </head>
 <body>
     <section>
